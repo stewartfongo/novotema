0657fcd311d995e5aba21aa7527b7fd55b116c64
Stewart Fonseca <art.of.art.fon.go.2012@gmail.com>
2018-08-21 18:19:26 +0000

    Inserção de header e footer

--- header.php
+++ header.php
@@ -4,14 +4,13 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Novo Tema</title>
+	<title><?php wp_title();?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" type="text/css" />
+	
+	<?php wp_head();?>
 </head>
 <body>
-<?php get_header();?>
-
-<h2>Seja bem-vindo ao novo tema</h2>
-
-<?php get_footer();?>
-
+    
+<?php wp_footer();?>
 </body>
 </html>
