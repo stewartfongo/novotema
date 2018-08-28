0a7fe89768057f5ee4b556679c7185b2e1757d39
Laiz Assucena <assucena_laiz@hotmail.com>
2018-08-28 18:29:46 +0000

    inserção da function

--- index.php
+++ index.php
@@ -1,10 +1,43 @@
 <?php get_header();?>
-<div class="row">
-	<div class="col-md-12">
-		<div class="jumbotron">
-			<h2>Seja bem-vindo ao novo tema</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="jumbotron">
+				<h2>Seja bem-vindo ao novo tema</h2>
+			</div>
 		</div>
 	</div>
-</div>
+	
+	<div class="row">
+		<?php
+			$cont = 0;
+				if (have_posts()){
+					while (have_posts()){
+						the_post();
+						$cont++;
+					if ($cont <- 3)) {
+		?>
+		
+					<div class="col-md-4">
+						
+						<h3><?php the_title(); ?></h3>
+						<div class="img-responsive img-thumbnail foto"><?php the_post_thumbnail(); ?></div>
+						<p><?php the_excerpt(); ?></p>
+	</div>
+	<?php
+					}
+					}
+				}
+				?>
+
+
+<?php
+	if ( have_posts()){
+		while (have_posts()){
+			the_post();
+			the_title();
+			the_excerpt();
+		} // fim do while
+	} // fim do if
+?>
 
 <?php get_footer();?>
