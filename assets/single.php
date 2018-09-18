5a1cd7b6e2629eac0fd81fc8d775f321a1ac0795
Julia Bastos <juliabastos00@hotmail.com>
2018-09-18 02:26:39 +0000

    Criação do single.php

---
+++ assets/single.php
@@ -0,0 +1,34 @@
+<?php get_header();?>
+
+<?php
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post();
+
+
+?>
+<div class="row">
+	<div class="col-md-12">
+		<div class="jumbotron">
+			<h2><?php the_title();?></h2>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<div class="jumbotron">
+			<h2><?php the_content();?></h2>
+		</div>
+	</div>
+</div>
+
+
+		
+<?php
+	}
+}
+?>
+
+<?php get_footer(); ?>
+
