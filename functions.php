48978883d763f4278fc5c12546b62c5f0bbd5075
Julia Bastos <juliabastos00@hotmail.com>
2018-09-18 02:27:55 +0000

    Alteração no functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,9 @@
 <?php
 
 add_theme_support('post-thumbnails');
+
+function registrar_menu_navegacao(){
+	register_nav_menu('header-menu','main_menu');
+}
+
+add_action('init','registrar_menu_navegacao');
